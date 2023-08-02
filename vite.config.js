a5f49bb4db7de836a9e85d35cd2b6c3b2fa45228
Carlos-vargs <cvargaslopez769@gmail.com>
2023-08-02 16:16:11 -0600

    build: configure aliases

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/App.jsx"],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@components": "/resources/js/components/",
+            "@theme": "/resources/js/theme/",
+            "@layouts": "/resources/js/layouts/",
+            "@pages": "/resources/js/pages/",
+            "@icons": "/resources/js/resources/icons/",
+            "@images": "/resources/js/resources/images/",
+            "@contexts": "/resources/js/contexts/",
+        },
+    },
 });
