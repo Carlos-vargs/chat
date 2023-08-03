6b538a3946acfba76bb1e003a26cca4e6886648b
Carlos-vargs <cvargaslopez769@gmail.com>
2023-08-03 08:16:58 -0600

    ci: add broadcast channels

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Conversation;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('chatsam', function (User $user) {
+    return $user->id;
+});
+
+Broadcast::channel('conversation.{conversation}', function ($user, Conversation $conversation) {
+    $isParticipant = $conversation->participants()->where('user_id', $user->id)->exists();
+    return $isParticipant;
 });
