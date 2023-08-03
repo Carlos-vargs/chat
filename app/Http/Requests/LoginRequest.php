e07defb52f8c048f0c0b83e94fe29943f33b9c3d
Carlos-vargs <cvargaslopez769@gmail.com>
2023-08-03 08:11:43 -0600

    ci: setup app request

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CheckPassword;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * new CheckPassword
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|string|email|exists:users,email',
+            'password' => ['required', 'numeric'],
+            'remember' => 'boolean',
+        ];
+    }
+}
