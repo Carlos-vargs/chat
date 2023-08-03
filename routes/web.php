a851206d15b3f5434ebffd1f0265eb91ca517aa7
Carlos-vargs <cvargaslopez769@gmail.com>
2023-08-03 08:26:45 -0600

    ci: add routes app

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\PasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/', [ChatController::class, 'index'])->name('home');
+
+    Route::post('/current-chat', [ChatController::class, 'show']);
+
+    Route::post('/create-group', [GroupController::class, 'store']);
+
+    Route::post('/send-message', [ChatController::class, 'store']);
+
+    Route::get('/change-password', [PasswordController::class, 'create'])->name('change-password');
+
+    Route::post('/logout', [LogoutController::class, 'destroy']);
 });
+
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
+
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/reset-password', [PasswordController::class, 'edit'])->name('reset-password');
